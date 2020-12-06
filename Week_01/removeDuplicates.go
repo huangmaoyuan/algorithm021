a21d32a3f2f376b35978bd0284756c7cfbc6eab5
huangmaoyuan <huangmaoyuan@huangmaoyuans-MacBook-Pro.local>
2020-12-06 13:11:51 +0800

    removeDuplicates

---
+++ Week_01/removeDuplicates.go
@@ -0,0 +1,13 @@
+func removeDuplicates(nums []int) int {
+    i:=0
+    j:=1
+    for j<len(nums){
+        if nums[i]==nums[j]{
+            j++
+        }else{
+            i++
+            nums[i]=nums[j]
+        }
+    }
+    return i+1
+}
