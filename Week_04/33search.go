77e14e670d2e63ac85880e55574353338c9b504d
huangmaoyuan <huangmaoyuan@huangmaoyuans-MacBook-Pro.local>
2020-12-27 12:14:09 +0800

    homework and notes

---
+++ Week_04/33search.go
@@ -0,0 +1,24 @@
+func search(nums []int, target int) int {
+    l,r:=0,len(nums)-1
+    var mid int
+    for l<=r{
+        mid=(l+r)/2
+        if target==nums[mid]{
+            return mid
+        }
+        if nums[mid]>=nums[l]{
+            if nums[l]<=target && target<nums[mid]{
+                r=mid-1
+            }else{
+                l=mid+1
+            }
+        }else{
+            if nums[mid]<target && target<=nums[r]{
+                l=mid+1
+            }else{
+                r=mid-1
+            }
+        }
+    }
+    return -1
+}
